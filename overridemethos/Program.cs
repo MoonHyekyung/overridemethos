88ee747f766ad6eb8507e456b4f4b5d91e726280
MoonHyekyung <moonsoft02@gmail.com>
2020-06-05 09:53:48 +0900

    초기 커밋.

---
+++ overridemethos/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethos
+{
+    
+    class Program
+    {
+        static int Power(int input)
+        {
+            return input * input;
+        }
+        static int Power(int input, int count)
+        {
+            int mul = input;
+            for (int i = 0; i < count; i++)
+                input *= mul;
+            return input;
+        }
+
+        static int SumAll(int end)
+        {
+            int sum = 0;
+            for (int i = 1; i <= end; i++) sum += i;
+            return sum;
+        }
+        static int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++) sum += i;
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int Power(int input): " + Power(5));
+            Console.WriteLine("int Power(int input, count): " + Power(5,3));
+            Console.WriteLine("int SumAll(int end): " + SumAll(10));
+            Console.WriteLine("int SumAll(int start, int end): " + SumAll(5,10));
+        }
+    }
+}
